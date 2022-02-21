c34f57363126017bbfab9df880888b2bb03d5457
Salah2Eddin <50306707+Salah2Eddin@users.noreply.github.com>
2022-02-22 01:25:43 +0200

    Added Comments

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Subtract a square Assignment1 - Task 2 - CS112
 # Rules
-This is a two-player mathematical game of strategy. It is played by two
+>This is a two-player mathematical game of strategy. It is played by two
 people with a pile of coins (or other tokens) between them. The players take turns removing
 coins from the pile, always removing a non-zero square number of coins (1, 4, 9, 16, …). The
 player who removes the last coin wins.
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+# Author: Salah Eddin Mohamed
+# Date: 22/2/2022
+# Institution: FCAI CU
+# Email: salaheddin.mohamed12345@gmail.com
+# Desc: This is a two-player mathematical
+# game of strategy. It is played by two people with a pile of coins (or other
+# tokens) between them. The players take turns removing coins from the pile,
+# always removing a non-zero square number of coins (1, 4, 9, 16, …). The
+# player who removes the last coin wins.
+from random import randint
+
+
 def check_if_possible(x, n):
     """
-    Checks if play x is possible
-    if its possible apply it then return true
+    Checks if move x is possible
     :param x: player input
     :param n: current number of coins
     :return: bool
@@ -15,16 +26,22 @@ def check_if_possible(x, n):
     return True
 
 
-coins = 100
+# This makes sure that the starting number of coins is random
+coins = randint(10, 1000)
+
+# This way, player changing is easier
+# this boolean is inversed after each move
+# and its value is used to get the player number
 cur_player = False
 player_name = {False: "Player 1", True: "Player 2"}
+
 while coins > 0:
     print("Current number of coins:", coins)
     player_input = int(input(f"{player_name[cur_player]}'s Turn: "))
     if check_if_possible(player_input, coins):
         coins -= player_input
         if coins != 0:
-            # reverse player after each move
+            # inverse player after each move
             cur_player = not cur_player
     else:
         print("Invalid Move")
