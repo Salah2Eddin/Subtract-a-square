e688d83bda4d959fd48ac45fd0521932c7114f0a
Salah2Eddin <50306707+Salah2Eddin@users.noreply.github.com>
2022-02-23 21:46:58 +0200

    Cleaned Code

--- main.py
+++ main.py
@@ -2,47 +2,61 @@
 # Date: 22/2/2022
 # Institution: FCAI CU
 # Email: salaheddin.mohamed12345@gmail.com
-# Desc: This is a two-player mathematical
-# game of strategy. It is played by two people with a pile of coins (or other
-# tokens) between them. The players take turns removing coins from the pile,
-# always removing a non-zero square number of coins (1, 4, 9, 16, …). The
-# player who removes the last coin wins.
 from random import randint
 
 
-def check_if_possible(x, n):
+def is_sqr(n):
+    """
+    Checks if a number is a square
+    :param n: the number to be checked
+    :return: boolean
+    """
+    i = 1
+    while i * i < n:
+        i += 1
+    return True if i * i == n else False
+
+
+def valid_move(x, n):
     """
     Checks if move x is possible
     :param x: player input
     :param n: current number of coins
-    :return: bool
+    :return: boolean
     """
-    i = 1
-    # checks if player input is a square and is less than total number of coins
-    while i * i != x:
-        i += 1
-        if i * i > x or i * i > n:
-            return False
-    return True
+    return True if is_sqr(x) and x <= n else False
 
 
-# This makes sure that the starting number of coins is random
-coins = randint(10, 1000)
+def main_loop():
+    """
+    The Game's Main Loop Function
+    :return: None
+    """
+    # This makes sure that the starting number of coins is random
+    coins = randint(10, 1000)
 
-# This way, player changing is easier
-# this boolean is inversed after each move
-# and its value is used to get the player number
-cur_player = False
-player_name = {False: "Player 1", True: "Player 2"}
+    cur_player = False
+    player_name = {False: "Player 1", True: "Player 2"}
 
-while coins > 0:
-    print("Current number of coins:", coins)
-    player_input = int(input(f"{player_name[cur_player]}'s Turn: "))
-    if check_if_possible(player_input, coins):
-        coins -= player_input
-        if coins != 0:
-            # inverse player after each move
-            cur_player = not cur_player
+    while coins > 0:
+        print("Current number of coins:", coins)
+
+        player_input = int(input(f"{player_name[cur_player]}'s Turn: "))
+
+        if valid_move(player_input, coins):
+            coins -= player_input
+            if coins != 0:
+                # change player after each move
+                cur_player = not cur_player
+        else:
+            print("Invalid Move")
+    print(player_name[cur_player], "Wins!")
+
+
+if __name__ == "__main__":
+    reset = "y"
+    while reset == "y":
+        main_loop()
+        reset = input("Want to play again? y/n: ").lower()
     else:
-        print("Invalid Move")
-print(player_name[cur_player], "Wins!")
+        print("Good Bye!")
